a81aa53fb8d354affe122d37f43927a19147f062
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2024-02-11 20:21:55 +0100

    update

--- libs/output-target-angular/src/output-angular.spec.ts
+++ libs/output-target-angular/src/output-angular.spec.ts
@@ -32,6 +32,7 @@ import type { Components, FileUploadRejectedFile } from 'component-library/compo
 
   it('should use a relative path to types when a component-library is not provided', () => {
     const outputTarget: OutputTargetAngular = {
+      componentCorePackage: 'component-library',
       directivesMetaFile: '../component-library-angular/src/meta.ts',
       directivesProxyFile: '../component-library-angular/src/proxies.ts',
       outputType: 'standalone',
@@ -44,7 +45,7 @@ import type { Components, FileUploadRejectedFile } from 'component-library/compo
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, NgZone, EventEmitter, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ProxyCmp, proxyOutputs } from './angular-component-lib/utils';
 
-import type { Components, FileUploadRejectedFile } from '../../design-system/dist/types/components';
+import type { Components, FileUploadRejectedFile } from 'component-library/components';
 
 `,
     )
