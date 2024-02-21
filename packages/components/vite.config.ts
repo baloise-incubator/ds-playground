31de11f850cf058c0e0f1f87fc75176d4b3aaba5
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2024-02-21 10:02:19 +0100

    fix testing

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/packages/styles',
+  cacheDir: '../../node_modules/.vite/packages/components',
 
   plugins: [vue(), nxViteTsPaths()],
 
@@ -13,8 +13,8 @@ export default defineConfig({
     globals: true,
     cache: { dir: '../../node_modules/.vitest' },
     environment: 'jsdom',
-    include: ['scripts/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
-    coverage: { reportsDirectory: '../../coverage/packages/styles', provider: 'v8' },
+    coverage: { reportsDirectory: '../../coverage/packages/components', provider: 'v8' },
   },
 })
